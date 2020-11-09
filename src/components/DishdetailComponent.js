65b740a2d9ac0b837e52342ef00e270df6c3d078
mangaka585 <mangaka585@gmail.com>
2020-11-10 01:35:48 +0300

    Assignment 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    renderComments(dish) {
+        if (dish != null) {
+            const commentElement = this.props.selectedDish.comments.map((comment) => {
+                return(
+                    <li key={comment.id}>
+                        <li className="mt-2">{comment.comment}</li>
+                        <li className="mt-2"> -- {comment.author}, {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: '2-digit'
+                        }).format(new Date(comment.date))}</li>
+                    </li>
+                );
+            })
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {commentElement}
+                    </ul>
+                </div>
+            );
+        } else
+            return(
+                <div></div>
+            );
+    }
+
+    render(){
+        return(
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.selectedDish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DishDetail;
